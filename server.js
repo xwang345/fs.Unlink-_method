88a921a2d23c72ce8bdc958c824bbe55b11a4a4e
Xiaochen Wang <ryan.wang23@hotmail.com>
2018-09-12 04:31:10 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var fs = require('fs');
+var filepath = 'testFile.txt';
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+let test = "Test file Test file Test file Test file Test file Test file Test file Test file Test file Test file Test file Test file";
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.use(express.static('public'));
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", (req,res) => {
+   res.sendFile(path.join(__dirname + "/views/home.html"));
+   console.log("!!!!!!")
+   fs.writeFile(filepath, test, (err) => {
+    if (err) throw err;
+    // success case, the file was saved
+    console.log('test' + filepath + 'create successfully!!');
+  });
+});
+
+app.get("/delete", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/delete.html"));
+   fs.unlink(filepath, (err)=> {
+    if (err) throw err;
+    // success case, the file was delete
+    console.log('File '+filepath+' delete successfully!');
+   })
+});
+
+fs.writeFile(filepath, test, (err) => {
+  if (err) throw err;
+  // success case, the file was saved
+  console.log('test' + filepath + 'create successfully!!');
+});
+
+
+// setup http server to listen on HTTP_PORT
+
+app.use(function(req, res) {
+  res.status(404).send("Sorry!!!!!!!>>>Page Not Found! <<<:(");
+});
+
+app.listen(HTTP_PORT, onHttpStart);
+
+app.use(express.static('public'));
