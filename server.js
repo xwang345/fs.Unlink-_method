ea172e06a1f8f71c6b0ac3bd428c293c20815798
Xiaochen Wang <ryan.wang23@hotmail.com>
2018-09-12 14:38:45 -0400

    Third commit

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var path = require("path");
 var fs = require('fs');
 var filepath = 'testFile.txt';
 const testFolder = './';
+const chalk = require('chalk');
+var countFiles = require('count-files')
+// const exphbs = require('express-handlebars');
+// const bodyParser = require('body-parser');
 
 
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 3000;
 
 let test = "Test file Test file Test file Test file Test file Test file Test file Test file Test file Test file Test file Test file";
 
@@ -16,22 +20,53 @@ function onHttpStart() {
 }
 
 app.use(express.static('public'));
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.engine(".hbs", exphbs({
+//     extname: ".hbs",
+//     defaultLayout: 'layout',
+//     helpers: {
+//         equal: (lvalue, rvalue, options) => {
+//             if (arguments.length < 3)
+//                 throw new Error("Handlebars Helper equal needs 2 parameters");
+//             if (lvalue != rvalue) {
+//                 return options.inverse(this);
+//             } else {
+//                 return options.fn(this);
+//             }
+//         }
+//     }
+// }));
+// app.set("view engine", ".hbs");
+
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", (req,res) => {
    res.sendFile(path.join(__dirname + "/views/home.html"));
-   console.log("!!!!!!")
+  //  res.send("hahahah");
    fs.writeFile(filepath, test, (err) => {
     if (err) throw err;
     // success case, the file was saved
     console.log( filepath + ' create successfully!!');
   });
-});
+  console.log(chalk.red("==================="))
+  var stats = countFiles(testFolder, function (err, results) {
+    console.log(chalk.yellow('done counting'));
+    console.log(results);// { files: 10, dirs: 2, bytes: 234 }
+    console.log(chalk.red("==========================================="))
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
   });
-})
+  setTimeout(function() {
+    fs.readdir(testFolder, (err, files) => {
+      console.log(chalk.red("==================="))
+      console.log(chalk.red("Here the file list"));
+      files.forEach(file => {
+        console.log(chalk.blue(file));
+      });
+      console.log(chalk.red("==================="))
+    })
+  }, 50);
+
+});
 
 app.get("/delete", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/delete.html"));
@@ -40,7 +75,37 @@ app.get("/delete", (req, res) => {
     // success case, the file was delete
     console.log(filepath+' delete successfully!');
    })
+   console.log(chalk.red("==================="))
+  var stats = countFiles(testFolder, function (err, results) {
+    console.log(chalk.yellow('done counting'));
+    console.log(results);// { files: 10, dirs: 2, bytes: 234 }
+    console.log(chalk.red("==========================================="))
+  });
+   setTimeout(function() {
+    fs.readdir(testFolder, (err, files) => {
+      console.log(chalk.red("==================="));
+      console.log(chalk.red("Here the file list after delete textFile.txt:"));
+      files.forEach(file => {
+        console.log(chalk.blue(file));
+      });
+      console.log(chalk.red("==================="));
+    })
+  }, 50);
 });
+
+fs.readdir(testFolder, (err, files) => {
+  console.log(chalk.red("==================="))
+  console.log(chalk.red("Here the file list:"))
+  files.forEach(file => {
+    console.log(chalk.blue(file));
+  });
+  console.log(chalk.red("==================="))
+  var stats = countFiles(testFolder, function (err, results) {
+    console.log(chalk.yellow('done counting'));
+    console.log(results);// { files: 10, dirs: 2, bytes: 234 }
+    console.log(chalk.red("==========================================="));
+  });
+})
 
 fs.writeFile(filepath, test, (err) => {
   if (err) throw err;
@@ -57,4 +122,4 @@ app.use(function(req, res) {
 
 app.listen(HTTP_PORT, onHttpStart);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
